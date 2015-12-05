0a241897cd16fcb9ac0ecafc7521801816dc318e
Erdong Feng <edfeng@thoughtworks.com>
2015-12-05 12:17:58 +0800

    turn on source map support. 'eval-source-map' will make the scripts exactly the same as in codebase; 'eval' has lower quality sourcemaps, but more effetient.

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,42 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
+var merge = require('webpack-merge');
 
+const TARGET = process.env.npm_lifecycle_event;
 const ROOT_PATH = path.resolve(__dirname);
 const APP_PATH = path.resolve(ROOT_PATH, 'app')
 const BUILD_PATH = path.resolve(ROOT_PATH, 'build')
 
-module.exports = {
+var common = {
   entry: APP_PATH,
   output: {
     path: BUILD_PATH,
     filename: 'bundle.js'
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Kanban app'
-    }),
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  devServer: {
-    historyApiFallback: true,
-    hot: true,
-    inline: true,
-    progess: true,
-    port: 4000
-  },
   module: {
-    loaders: [
-      {
-        test: /\.css$/,
-        loaders: ['style', 'css'],
-        include: APP_PATH
-      }
-    ]
+    loaders: [{
+      test: /\.css$/,
+      loaders: ['style', 'css'],
+      include: APP_PATH
+    }]
   }
-};
+}
+if (TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval',
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Kanban app'
+      }),
+      new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progess: true,
+      port: 4000
+    },
+  });
+}
